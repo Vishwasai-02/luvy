700d7255e893c6754304ecf72bdfdd6c9f330456
vishwasai-02 <vishwasainarishetti@gmail.com>
2026-05-14 22:36:04 +0530

    side scrolling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -570,6 +570,28 @@ const Contact = () => {
                 </div>
               </div>
             </div>
+
+            {/* Map */}
+            <div style={{
+              marginTop: '30px',
+              borderRadius: '12px',
+              overflow: 'hidden',
+              boxShadow: '0 15px 40px rgba(0, 0, 0, 0.1)',
+              maxWidth: '420px',
+              height: '320px',
+              border: '1px solid #eee'
+            }}>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241316.64332468755!2d72.7410986518111!3d19.082522321481238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
+                width="100%" 
+                height="100%" 
+                style={{ border: 0, display: 'block' }} 
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Luxe Jewels Location"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
